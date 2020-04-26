07382ef74198edc1489fc9236ed60f10890cf197
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-04-26 22:56:25 +0530

    raw agent matching ids

---
+++ problem1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main(){
+
+int n;
+cout<<"enter the number of names tht u wanna enter"<<endl;
+cin>>n;
+cout<<"name string:"<<endl;
+string s1,s2,s3,s4;
+cin>>s1;
+cin>>s2;
+cin>>s3;
+cin>>s4;
+if(s1=="dhruvi")
+	cout<<"yes"<<endl;
+else if(s2=="dhruvi")
+	cout<<"yes"<<endl;
+else if(s3=="dhruvi")
+	cout<<"yes"<<endl;
+else if(s4=="dhruvi")
+	cout<<"yes"<<endl;
+else 
+	cout<<"no";
+
+return 0;
+}
