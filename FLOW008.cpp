87cc71671baef2a88b735206ad6f4889546a37af
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-04-28 11:36:58 +0530

    Create FLOW008.cpp

---
+++ FLOW008.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <sstream> 
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+     int N;
+     cin>>N;
+     
+         if(N<10&&N>-20)
+         cout<<"What an obedient servant you are!"<<endl;
+       
+else
+         cout<<"-1"<<endl;
+    }     
+return 0;
+    
+}
